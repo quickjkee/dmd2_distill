544523377b545540c3a15a36204469baca9defe8
quickjkee <jkee2013@mail.ru>
2024-09-14 15:51:13 +0300

    init

--- main/test_folder_sd.py
+++ main/test_folder_sd.py
@@ -65,7 +65,7 @@ def get_x0_from_noise(sample, model_output, timestep):
 
 
 @torch.no_grad()
-def log_validation(tokenizer, vae, text_encoder, current_model):
+def log_validation(accelerator, tokenizer, vae, text_encoder, current_model, args):
     validation_prompts = [
         "portrait photo of a girl, photograph, highly detailed face, depth of field, moody light, golden hour, style by Dan Winters, Russell James, Steve McCurry, centered, extremely detailed, Nikon D850, award winning photography",
         "Self-portrait oil painting, a beautiful cyborg with golden hair, 8k",
@@ -83,6 +83,7 @@ def log_validation(tokenizer, vae, text_encoder, current_model):
     ]
 
     image_logs = []
+    generator = torch.Generator().manual_seed(args.seed)
 
     for _, prompt in enumerate(validation_prompts):
         text_input_ids_one = tokenizer(
@@ -92,7 +93,28 @@ def log_validation(tokenizer, vae, text_encoder, current_model):
             truncation=True,
             return_tensors="pt",
         ).input_ids
-        text_embedding = text_encoder(text_input_ids_one)
+        text_input_ids_one = text_input_ids_one.to(accelerator.device).reshape(-1, text_input_ids_one.shape[-1])
+        text_embedding = text_encoder(text_input_ids_one)[0]
+        noise = torch.randn(len(text_embedding), 4,
+                            args.latent_resolution, args.latent_resolution,
+                            dtype=torch.float32,
+                            generator=generator,
+                            ).to(accelerator.device)
+        timesteps = torch.ones(len(text_embedding), device=accelerator.device, dtype=torch.long)
+
+        eval_images = current_model(
+            noise, timesteps.long() * (args.num_train_timesteps - 1), text_embedding
+        ).sample
+
+        eval_images = get_x0_from_noise(
+            noise, eval_images, timesteps
+        )
+
+        # decode the latents and cast to uint8 RGB
+        vae = vae.to(eval_images.dtype)
+        eval_images = vae.decode(eval_images * 1 / 0.18215).sample.float()
+        eval_images = ((eval_images + 1.0) * 127.5).clamp(0, 255).to(torch.uint8).permute(0, 2, 3, 1)
+        eval_images = eval_images.contiguous()
 
 
 @torch.no_grad()
--- main/train_sd.py
+++ main/train_sd.py
@@ -553,7 +553,7 @@ class Trainer:
                                   dataloader=self.eval_dataloader,
                                   args=args)
             self.model.feedforward_model.train()
-            self.model.vae = self.model.vae.to(self.model.feedforward_model.dtype)
+            self.model.vae = self.model.vae.to(torch.float16)
 
             if accelerator.is_main_process:
                 visualize_images = sampled_data['all_images'][:args.test_visual_batch_size]
